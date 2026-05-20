dc6d936685fcb6d1ac44150d9a7d75d7a8ad40ad
Angela S. Milford <angela.milford123@gmail.com>
2026-05-19 16:37:01 -1100

    Outputted a list of recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.ts
+++ src/app/recipes/recipe-list/recipe-list.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeList {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.heartfoundation.org.nz/images/all-shared-sections/recipes/cocoa-rice.webp?mtime=1669000875'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.heartfoundation.org.nz/images/all-shared-sections/recipes/cocoa-rice.webp?mtime=1669000875')
   ];
 
