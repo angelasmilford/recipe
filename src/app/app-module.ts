f20826fc52570c217f9d643eb348ccdcd4f06773
Angela S. Milford <angela.milford123@gmail.com>
2026-06-05 23:35:00 -1100

    Fixed Recipe Item component import path in App module

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,7 +7,7 @@ import { Header } from './header/header';
 import { Recipes } from './recipes/recipes';
 import { RecipeList } from './recipes/recipe-list/recipe-list';
 import { RecipeDetail } from './recipes/recipe-detail/recipe-detail';
-import { RecipeItem } from './recipes/recipe-item/recipe-item';
+import { RecipeItem } from './recipes/recipe-list/recipe-item/recipe-item';
 import { ShoppingList } from './shopping-list/shopping-list';
 import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit';
 import { DropdownDirective } from './shared/dropdown.directive';
