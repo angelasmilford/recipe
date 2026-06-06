d30770f43363cf2b1b1c9130c207981d5b2234fb
Angela S. Milford <angela.milford123@gmail.com>
2026-06-05 23:32:11 -1100

    Moved Recipe Item component code into Recipe List and removed the separate Recipe Item component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './recipe-item.html',
   styles: ``,
 })
-export class RecipeItem {}
+export class RecipeItem {
+  @Input() recipe: Recipe; 
+
+  constructor(private recipeService: RecipeService) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
+}
