2107e0d8ea4a2144699359956a529e68485d5f17
Angela S. Milford <angela.milford123@gmail.com>
2026-06-09 01:29:58 -1100

    Configured Route Parameters

--- src/app/recipes/recipe-detail/recipe-detail.ts
+++ src/app/recipes/recipe-detail/recipe-detail.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -10,12 +11,21 @@ import { RecipeService } from '../recipe.service';
   styles: ``,
 })
 export class RecipeDetail {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
   }
 
   onAddToShoppingList() {
--- src/app/recipes/recipe-list/recipe-item/recipe-item.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -12,15 +11,7 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItem {
   @Input() recipe: Recipe; 
 
-  constructor(private recipeService: RecipeService) {
-
-  }
-
   ngOnInit() {
 
-  }
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
