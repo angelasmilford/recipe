2cf1ec4aaa76da387b73ce0a01feab176865f5b5
Angela S. Milford <angela.milford123@gmail.com>
2026-06-09 15:43:18 -1100

    Programmatic Navigation to the Edit Page

--- src/app/recipes/recipe-detail/recipe-detail.ts
+++ src/app/recipes/recipe-detail/recipe-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -15,7 +15,8 @@ export class RecipeDetail {
   id: number;
 
   constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute
+              private route: ActivatedRoute,
+              private router: Router
   ) { }
 
   ngOnInit() {
@@ -30,5 +31,10 @@ export class RecipeDetail {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 }
