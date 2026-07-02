c15b265233cf896f9f2051db22d324a940e725c1
Angela S. Milford <angela.milford123@gmail.com>
2026-07-01 13:49:23 -1100

    Transformed Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,31 +9,33 @@ import { Subject } from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Koko Araisa', 
-            'Samoan Cocoa Rice', 
-            'https://assets.heartfoundation.org.nz/images/all-shared-sections/recipes/cocoa-rice.webp?mtime=1669000875',
-            [
-                new Ingredient('White Rice', 50),
-                new Ingredient('Koko Samoa', 1),
-                new Ingredient('Water', 8),
-                new Ingredient('Coconut milk', 1),
-                new Ingredient('Sugar', 100)
-            ]
-        ),
-        new Recipe(
-            'Roti', 
-            'Fiji Indian Chicken Curry Roti Wrap', 
-            'https://www.polynesia.com/contentassets/b4851aaa304f4318b5fd095798fef4b5/easy-to-grab-and-go-or-tasty-enough-for-a-sit-down-meal.webp',
-            [
-                new Ingredient('Flour', 2),
-                new Ingredient('Salt', 1),
-                new Ingredient('Water', 4),
-                new Ingredient('Butter', 6)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Koko Araisa', 
+    //         'Samoan Cocoa Rice', 
+    //         'https://assets.heartfoundation.org.nz/images/all-shared-sections/recipes/cocoa-rice.webp?mtime=1669000875',
+    //         [
+    //             new Ingredient('White Rice', 50),
+    //             new Ingredient('Koko Samoa', 1),
+    //             new Ingredient('Water', 8),
+    //             new Ingredient('Coconut milk', 1),
+    //             new Ingredient('Sugar', 100)
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Roti', 
+    //         'Fiji Indian Chicken Curry Roti Wrap', 
+    //         'https://www.polynesia.com/contentassets/b4851aaa304f4318b5fd095798fef4b5/easy-to-grab-and-go-or-tasty-enough-for-a-sit-down-meal.webp',
+    //         [
+    //             new Ingredient('Flour', 2),
+    //             new Ingredient('Salt', 1),
+    //             new Ingredient('Water', 4),
+    //             new Ingredient('Butter', 6)
+    //         ]
+    //     )
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
 
