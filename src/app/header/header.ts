7c5666e3550c31a6917085fa45fd9ba524533339
Angela S. Milford <angela.milford123@gmail.com>
2026-07-01 13:22:19 -1100

    Stored Recipes

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class Header {
+    constructor(private dataStorageService: DataStorageService) {}
 
+    onSaveData() {
+        this.dataStorageService.storeRecipes();
+    }
 }
